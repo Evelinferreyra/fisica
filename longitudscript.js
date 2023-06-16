48420f8ded0129a71fe8edacd0e765fa12125ccd
juliaferreiro <115805432+juliaferreiro@users.noreply.github.com>
2023-06-16 12:04:30 -0300

    Update longitudscript.js

--- longitudscript.js
+++ longitudscript.js
@@ -17,31 +17,156 @@ function convertir(){
     numero = valor.value;
     numero = parseFloat(numero);
 
+    
     if (unidad1.value == "Kilometro" && unidad2.value== "Kilometro"){
         resultado = numero * 1;
         txtResultado.innerHTML = numero + " Kilometros/s equivalen a " + resultado + "Kilometros/s";
     }
+    if (unidad1.value == "Kilometro" && unidad2.value== "Hectometro"){
+            resultado = numero * 10;
+            txtResultado.innerHTML = numero + " Kilometros/s equivalen a " + resultado + "Hectómetro/s";
+    }
+    if (unidad1.value == "Kilometro" && unidad2.value== "Decametro"){
+        resultado = numero * 100;
+        txtResultado.innerHTML = numero + " Kilometros/s equivalen a " + resultado + "Decámetro/s";
+    }   
     if (unidad1.value == "Kilometro" && unidad2.value== "Metro"){
         resultado = numero * 1000;
         txtResultado.innerHTML = numero + " Kilometros/s equivalen a " + resultado + "Metro/s";
     }
-    if (unidad1.value == "Kilometro" && unidad2.value== "Centimetro"){
+    if (unidad1.value == "Kilometro" && unidad2.value== "Decimetro"){
         resultado = numero * 10000;
-        txtResultado.innerHTML = numero + " Kilometros/s equivalen a " + resultado + "Centimetro/s";
+        txtResultado.innerHTML = numero + " Kilometros/s equivalen a " + resultado + "Decíemetro/s";
+    }
+    if (unidad1.value == "Kilometro" && unidad2.value== "Centimetro"){
+        resultado = numero * 100000;
+        txtResultado.innerHTML = numero + " Kilometros/s equivalen a " + resultado + "Centímetro/s";
+    }
+    if (unidad1.value == "Kilometro" && unidad2.value== "Milmetro"){
+        resultado = numero * 1000000;
+        txtResultado.innerHTML = numero + " Kilometros/s equivalen a " + resultado + "Milimetro/s";
     }
 
-    
-    if (unidad1.value == "Metro" && unidad2.value== "Kilometro"){
-        resultado = numero / 1;
-        txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Kilometro/s";
+
+    if (unidad1.value == "Hectometro" && unidad2.value== "Kilometro"){
+        resultado = numero / 10;
+        txtResultado.innerHTML = numero + " Hectometro/s equivalen a " + resultado + "Kilometros/s";
     }
+    if (unidad1.value == "Hectometro" && unidad2.value== "Hectometro"){
+            resultado = numero * 1
+            txtResultado.innerHTML = numero + " Hectometro/s equivalen a " + resultado + "Hectómetro/s";
+    }
+    if (unidad1.value == "Hectometro" && unidad2.value== "Decametro"){
+        resultado = numero * 10;
+        txtResultado.innerHTML = numero + " Hectometro/s equivalen a " + resultado + "Decámetro/s";
+    }   
+    if (unidad1.value == "Hectometro" && unidad2.value== "Metro"){
+        resultado = numero * 100;
+        txtResultado.innerHTML = numero + " Hectometro/s equivalen a " + resultado + "Metro/s";
+    }
+    if (unidad1.value == "Hectometro" && unidad2.value== "Decimetro"){
+        resultado = numero * 1000;
+        txtResultado.innerHTML = numero + " Hectometro/s equivalen a " + resultado + "Decíemetro/s";
+    }
+    if (unidad1.value == "Hectometro" && unidad2.value== "Centimetro"){
+        resultado = numero * 10000;
+        txtResultado.innerHTML = numero + " Hectometro/s equivalen a " + resultado + "Centímetro/s";
+    }
+    if (unidad1.value == "Hectometro" && unidad2.value== "Milmetro"){
+        resultado = numero * 100000;
+        txtResultado.innerHTML = numero + " Hectometro/s equivalen a " + resultado + "Milimetro/s";
+    }
+
+
+    if (unidad1.value == "Decametro" && unidad2.value== "Kilometro"){
+        resultado = numero / 100;
+        txtResultado.innerHTML = numero + " Decametro/s equivalen a " + resultado + "Kilometros/s";
+    }
+    if (unidad1.value == "Decametro" && unidad2.value== "Hectometro"){
+            resultado = numero / 10;
+            txtResultado.innerHTML = numero + " Decametro/s equivalen a " + resultado + "Hectómetro/s";
+    }
+    if (unidad1.value == "Decametro" && unidad2.value== "Decametro"){
+        resultado = numero * 1;
+        txtResultado.innerHTML = numero + " Decametro/s equivalen a " + resultado + "Decámetro/s";
+    }   
+    if (unidad1.value == "Decametro" && unidad2.value== "Metro"){
+        resultado = numero * 10;
+        txtResultado.innerHTML = numero + " Decametro/s equivalen a " + resultado + "Metro/s";
+    }
+    if (unidad1.value == "Decametro" && unidad2.value== "Decimetro"){
+        resultado = numero * 100;
+        txtResultado.innerHTML = numero + " Decametro/s equivalen a " + resultado + "Decíemetro/s";
+    }
+    if (unidad1.value == "Decametro" && unidad2.value== "Centimetro"){
+        resultado = numero * 1000;
+        txtResultado.innerHTML = numero + " Decametro/s equivalen a " + resultado + "Centímetro/s";
+    }
+    if (unidad1.value == "Decametro" && unidad2.value== "Milmetro"){
+        resultado = numero * 10000;
+        txtResultado.innerHTML = numero + " Decametro/s equivalen a " + resultado + "Milimetro/s";
+    }
+
+
+
+    if (unidad1.value == "Metro" && unidad2.value== "Kilometro"){
+        resultado = numero / 1000;
+        txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Kilometros/s";
+    }
+    if (unidad1.value == "Metro" && unidad2.value== "Hectometro"){
+            resultado = numero / 100;
+            txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Hectómetro/s";
+    }
+    if (unidad1.value == "Metro" && unidad2.value== "Decametro"){
+        resultado = numero / 10;
+        txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Decámetro/s";
+    }   
     if (unidad1.value == "Metro" && unidad2.value== "Metro"){
         resultado = numero * 1;
         txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Metro/s";
     }
+    if (unidad1.value == "Metro" && unidad2.value== "Decimetro"){
+        resultado = numero * 10;
+        txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Decíemetro/s";
+    }
     if (unidad1.value == "Metro" && unidad2.value== "Centimetro"){
         resultado = numero * 100;
-        txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Centimetro/s";
+        txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Centímetro/s";
+    }
+    if (unidad1.value == "Metro" && unidad2.value== "Milmetro"){
+        resultado = numero * 1000;
+        txtResultado.innerHTML = numero + " Metro/s equivalen a " + resultado + "Milimetro/s";
+    }
+
+
+
+    if (unidad1.value == "Decimetro" && unidad2.value== "Kilometro"){
+        resultado = numero / 10000;
+        txtResultado.innerHTML = numero + " Decimetro/s equivalen a " + resultado + "Kilometros/s";
+    }
+    if (unidad1.value == "Decimetro" && unidad2.value== "Hectometro"){
+            resultado = numero / 1000;
+            txtResultado.innerHTML = numero + " Decimetro/s equivalen a " + resultado + "Hectómetro/s";
+    }
+    if (unidad1.value == "Decimetro" && unidad2.value== "Decametro"){
+        resultado = numero / 100;
+        txtResultado.innerHTML = numero + " Decimetro/s equivalen a " + resultado + "Decámetro/s";
+    }   
+    if (unidad1.value == "Decimetro" && unidad2.value== "Metro"){
+        resultado = numero / 10;
+        txtResultado.innerHTML = numero + " Decimetro/s equivalen a " + resultado + "Metro/s";
+    }
+    if (unidad1.value == "Decimetro" && unidad2.value== "Decimetro"){
+        resultado = numero * 1;
+        txtResultado.innerHTML = numero + " Decimetro/s equivalen a " + resultado + "Decíemetro/s";
+    }
+    if (unidad1.value == "Decimetro" && unidad2.value== "Centimetro"){
+        resultado = numero * 10;
+        txtResultado.innerHTML = numero + " Decimetro/s equivalen a " + resultado + "Centímetro/s";
+    }
+    if (unidad1.value == "Decimetro" && unidad2.value== "Milmetro"){
+        resultado = numero * 100;
+        txtResultado.innerHTML = numero + " Decimetro/s equivalen a " + resultado + "Milimetro/s";
     }
 
 
@@ -49,12 +174,60 @@ function convertir(){
         resultado = numero / 100000;
         txtResultado.innerHTML = numero + " Centimetro/s equivalen a " + resultado + "Kilometros/s";
     }
+    if (unidad1.value == "Centimetro" && unidad2.value== "Hectometro"){
+            resultado = numero / 10000;
+            txtResultado.innerHTML = numero + " Centimetro/s equivalen a " + resultado + "Hectómetro/s";
+    }
+    if (unidad1.value == "Centimetro" && unidad2.value== "Decametro"){
+        resultado = numero / 1000;
+        txtResultado.innerHTML = numero + " Centimetro/s equivalen a " + resultado + "Decámetro/s";
+    }   
     if (unidad1.value == "Centimetro" && unidad2.value== "Metro"){
         resultado = numero / 100;
         txtResultado.innerHTML = numero + " Centimetro/s equivalen a " + resultado + "Metro/s";
     }
+    if (unidad1.value == "Centimetro" && unidad2.value== "Decimetro"){
+        resultado = numero / 10;
+        txtResultado.innerHTML = numero + " Centimetro/s equivalen a " + resultado + "Decíemetro/s";
+    }
     if (unidad1.value == "Centimetro" && unidad2.value== "Centimetro"){
         resultado = numero * 1;
-        txtResultado.innerHTML = numero + " Centimetro/s equivalen a " + resultado + "Centimetro/s";
+        txtResultado.innerHTML = numero + " Centimetro/s equivalen a " + resultado + "Centímetro/s";
     }
+    if (unidad1.value == "Centimetro" && unidad2.value== "Milmetro"){
+        resultado = numero * 10;
+        txtResultado.innerHTML = numero + " Centimetro/s equivalen a " + resultado + "Milimetro/s";
+    }
+
+    
+    
+    if (unidad1.value == "Milimetro" && unidad2.value== "Kilometro"){
+        resultado = numero / 1000000;
+        txtResultado.innerHTML = numero + " Milimetro/s equivalen a " + resultado + "Kilometros/s";
+    }
+    if (unidad1.value == "Milimetro" && unidad2.value== "Hectometro"){
+            resultado = numero / 100000;
+            txtResultado.innerHTML = numero + " Milimetro/s equivalen a " + resultado + "Hectómetro/s";
+    }
+    if (unidad1.value == "Milimetro" && unidad2.value== "Decametro"){
+        resultado = numero / 10000;
+        txtResultado.innerHTML = numero + " Milimetro/s equivalen a " + resultado + "Decámetro/s";
+    }   
+    if (unidad1.value == "Milimetro" && unidad2.value== "Metro"){
+        resultado = numero / 1000;
+        txtResultado.innerHTML = numero + " Milimetro/s equivalen a " + resultado + "Metro/s";
+    }
+    if (unidad1.value == "Milimetro" && unidad2.value== "Decimetro"){
+        resultado = numero / 100;
+        txtResultado.innerHTML = numero + " Milimetro/s equivalen a " + resultado + "Decíemetro/s";
+    }
+    if (unidad1.value == "Milimetro" && unidad2.value== "Centimetro"){
+        resultado = numero / 10;
+        txtResultado.innerHTML = numero + " Milimetro/s equivalen a " + resultado + "Centímetro/s";
+    }
+    if (unidad1.value == "Milimetro" && unidad2.value== "Milmetro"){
+        resultado = numero * 1;
+        txtResultado.innerHTML = numero + " Milimetro/s equivalen a " + resultado + "Milimetro/s";
+    }
+   
 }
