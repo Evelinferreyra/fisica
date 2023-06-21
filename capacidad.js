d07b4b2907cc138613fc5a0b83c5b94407f8bede
Evelinferreyra <115805581+Evelinferreyra@users.noreply.github.com>
2023-06-21 10:02:50 -0300

    Update capacidad.js

--- capacidad.js
+++ capacidad.js
@@ -42,12 +42,11 @@ function convertir(){
         resultado = numero * 1000;
         txtResultado.innerHTML = numero + " kilolitro/s equivalen a " + resultado + "decilitro/s";
     }
-}
-if (unidad1.value == "kilolitro" && unidad2.value== "hectolitros"){
-    resultado = numero * 10;
-    txtResultado.innerHTML = numero + " kilolitro/s equivalen a " + resultado + "hectolitro/s";
+    if (unidad1.value == "kilolitro" && unidad2.value== "hectolitros"){
+        resultado = numero * 10;
+        txtResultado.innerHTML = numero + " kilolitro/s equivalen a " + resultado + "hectolitro/s";
+    }
 
-}
 //-------------------kilolitro------------------------------------//
     
 //--------------------------litro---------------------------------//  
@@ -63,62 +62,61 @@ if (unidad1.value == "kilolitro" && unidad2.value== "hectolitros"){
         resultado = numero * 100;
         txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "Centilitro/s";
     }
-
-if (unidad1.value == "litro" && unidad2.value== "hectolitro"){
-    resultado = numero / 100;
-    txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "hectolitro/s";
-
-}
-
-if (unidad1.value == "litro" && unidad2.value== "decilitro"){
-    resultado = numero * 10;
-    txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "decilitro/s";
-}
-
-if (unidad1.value == "litro" && unidad2.value== "decalitro"){
-    resultado = numero *1000;
-    txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "decalitro/s";
-
-}
-
-if (unidad1.value == "litro" && unidad2.value== "mililitro"){
-    resultado = numero * 100;
-    txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "mililitro/s";
-}
-//-------------------------litro-------------------------------------//
-//----------------------------centilitro---------------------------
-
-    if (unidad1.value == "Centilitro" && unidad2.value== "Kilolitro"){
-        resultado = numero / 100000;
-        txtResultado.innerHTML = numero + " Centilitros/s equivalen a " + resultado + "Kilolitros/s";
-    }
-    if (unidad1.value == "Centilitro" && unidad2.value== "litro"){
+    if (unidad1.value == "litro" && unidad2.value== "hectolitro"){
         resultado = numero / 100;
-        txtResultado.innerHTML = numero + " Centilitro/s equivalen a " + resultado + "litro/s";
+        txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "hectolitro/s";
     }
-    if (unidad1.value == "Centilitro" && unidad2.value== "Centilitro"){
-        resultado = numero * 1;
-        txtResultado.innerHTML = numero + " Centilitro/s equivalen a " + resultado + "Centiltro/s";  
+    if (unidad1.value == "litro" && unidad2.value== "decilitro"){
+        resultado = numero * 10;
+        txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "decilitro/s";
+    }
+    
+    if (unidad1.value == "litro" && unidad2.value== "decalitro"){
+        resultado = numero *1000;
+        txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "decalitro/s";
+    
+    }
+    
+    if (unidad1.value == "litro" && unidad2.value== "mililitro"){
+        resultado = numero * 100;
+        txtResultado.innerHTML = numero + " litro/s equivalen a " + resultado + "mililitro/s";
+    }
+    //-------------------------litro-------------------------------------//
+    //----------------------------centilitro---------------------------
+    
+        if (unidad1.value == "Centilitro" && unidad2.value== "Kilolitro"){
+            resultado = numero / 100000;
+            txtResultado.innerHTML = numero + " Centilitros/s equivalen a " + resultado + "Kilolitros/s";
+        }
+        if (unidad1.value == "Centilitro" && unidad2.value== "litro"){
+            resultado = numero / 100;
+            txtResultado.innerHTML = numero + " Centilitro/s equivalen a " + resultado + "litro/s";
+        }
+        if (unidad1.value == "Centilitro" && unidad2.value== "Centilitro"){
+            resultado = numero * 1;
+            txtResultado.innerHTML = numero + " Centilitro/s equivalen a " + resultado + "Centiltro/s";  
+        }
+    
+        if (unidad1.value == "centilitro" && unidad2.value== "hectaltro"){
+        resultado = numero / 1000;
+        txtResultado.innerHTML = numero + " centilitro/s equivalen a " + resultado + "hectalitro/s";
+    
+    }
+    
+    if (unidad1.value == "centilitro" && unidad2.value== "mililitro"){
+        resultado = numero * 10;
+        txtResultado.innerHTML = numero + " centilitro/s equivalen a " + resultado + "mililitro/s";
+    }
+    if (unidad1.value == "centilitro" && unidad2.value== "decalitro"){
+        resultado = numero / 1000;
+        txtResultado.innerHTML = numero + " centilitro/s equivalen a " + resultado + "decalitro/s";
+    }
+    if (unidad1.value == "centilitro" && unidad2.value== "deciloitro"){
+        resultado = numero / 10;
+        txtResultado.innerHTML = numero + " centilitro/s equivalen a " + resultado + "decilitro/s";
     }
 
-    if (unidad1.value == "centilitro" && unidad2.value== "hectaltro"){
-    resultado = numero / 1000;
-    txtResultado.innerHTML = numero + " centilitro/s equivalen a " + resultado + "hectalitro/s";
 
-}
-
-if (unidad1.value == "centilitro" && unidad2.value== "mililitro"){
-    resultado = numero * 10;
-    txtResultado.innerHTML = numero + " centilitro/s equivalen a " + resultado + "mililitro/s";
-}
-if (unidad1.value == "centilitro" && unidad2.value== "decalitro"){
-    resultado = numero / 1000;
-    txtResultado.innerHTML = numero + " centilitro/s equivalen a " + resultado + "decalitro/s";
-}
-if (unidad1.value == "centilitro" && unidad2.value== "deciloitro"){
-    resultado = numero / 10;
-    txtResultado.innerHTML = numero + " centilitro/s equivalen a " + resultado + "decilitro/s";
-}
 
 //-----------------------centilitro-----------------------------//
 
@@ -166,87 +164,86 @@ if (unidad1.value == "centilitro" && unidad2.value== "deciloitro"){
     if (unidad1.value == "decilitro" && unidad2.value== "hectaltro"){
     resultado = numero / 1000;
     txtResultado.innerHTML = numero + " decilitro/s equivalen a " + resultado + "hectalitro/s";
+    }
 
-}
-
-if (unidad1.value == "decilitro" && unidad2.value== "mililitro"){
-    resultado = numero * 100;
-    txtResultado.innerHTML = numero + " decilitro/s equivalen a " + resultado + "mililitro/s";
-}
-if (unidad1.value == "decilitro" && unidad2.value== "decalitro"){
-    resultado = numero / 100;
-    txtResultado.innerHTML = numero + " decilitros/s equivalen a " + resultado + "decalitro/s";
-}
-if (unidad1.value == "decilitro" && unidad2.value== "decilitro"){
-    resultado = numero / 1;
-    txtResultado.innerHTML = numero + " decilitros/s equivalen a " + resultado + "decilitro/s";
-}
-//------------------------------------decilitro----------------------------------//
-if (unidad1.value == "decalitro" && unidad2.value== "Kilolitro"){
-    resultado = numero / 100;
-    txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "Kilolitros/s";
-}
-if (unidad1.value == "decalitro" && unidad2.value== "litro"){
-    resultado = numero *1;
-    txtResultado.innerHTML = numero + " Centilitro/s equivalen a " + resultado + "litro/s";
-}
-if (unidad1.value == "decalitro" && unidad2.value== "Centilitro"){
-    resultado = numero * 1000;
-    txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "Centiltro/s";  
-}
-
-if (unidad1.value == "decalitro" && unidad2.value== "hectaltro"){
-resultado = numero / 10;
-txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "hectalitro/s";
-
-}
-
-if (unidad1.value == "decalitro" && unidad2.value== "mililitro"){
-resultado = numero * 1000;
-txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "mililitro/s";
-}
-if (unidad1.value == "decalitro" && unidad2.value== "decalitro"){
-resultado = numero / 1;
-txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "decalitro/s";
-}
-if (unidad1.value == "decalitro" && unidad2.value== "deciloitro"){
-resultado = numero * 100;
-txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "decilitro/s";
-}
-//---------------------------------decalitro----------------------//
-//-----------------------------mililitro---------------------------//
-if (unidad1.value == "mililitro" && unidad2.value== "Kilolitro"){
-    resultado = numero / 100000;
-    txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "Kilolitros/s";
-}
-if (unidad1.value == "mililitro" && unidad2.value== "litro"){
-    resultado = numero / 1000;
-    txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "litro/s";
-}
-if (unidad1.value == "mililitro" && unidad2.value== "Centilitro"){
+    if (unidad1.value == "decilitro" && unidad2.value== "mililitro"){
+        resultado = numero * 100;
+        txtResultado.innerHTML = numero + " decilitro/s equivalen a " + resultado + "mililitro/s";
+    }
+    if (unidad1.value == "decilitro" && unidad2.value== "decalitro"){
+        resultado = numero / 100;
+        txtResultado.innerHTML = numero + " decilitros/s equivalen a " + resultado + "decalitro/s";
+    }
+    if (unidad1.value == "decilitro" && unidad2.value== "decilitro"){
+        resultado = numero / 1;
+        txtResultado.innerHTML = numero + " decilitros/s equivalen a " + resultado + "decilitro/s";
+    }
+    //------------------------------------decilitro----------------------------------//
+    if (unidad1.value == "decalitro" && unidad2.value== "Kilolitro"){
+        resultado = numero / 100;
+        txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "Kilolitros/s";
+    }
+    if (unidad1.value == "decalitro" && unidad2.value== "litro"){
+        resultado = numero *1;
+        txtResultado.innerHTML = numero + " Centilitro/s equivalen a " + resultado + "litro/s";
+    }
+    if (unidad1.value == "decalitro" && unidad2.value== "Centilitro"){
+        resultado = numero * 1000;
+        txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "Centiltro/s";  
+    }
+    
+    if (unidad1.value == "decalitro" && unidad2.value== "hectaltro"){
     resultado = numero / 10;
-    txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "Centiltro/s";  
-}
-
-if (unidad1.value == "mililitro" && unidad2.value== "hectaltro"){
-resultado = numero / 100000;
-txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "hectalitro/s";
-
-}
-
-if (unidad1.value == "mililitro" && unidad2.value== "mililitro"){
-resultado = numero * 1;
-txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "mililitro/s";
-}
-if (unidad1.value == "mililitro" && unidad2.value== "decalitro"){
-resultado = numero / 1000;
-txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "decalitro/s";
-}
-if (unidad1.value == "mililitro" && unidad2.value== "deciloitro"){
-resultado = numero / 100;
-txtResultado.innerHTML = numero + " mililitros/s equivalen a " + resultado + "decilitro/s";
-}
-
+    txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "hectalitro/s";
+    
+    }
+    
+    if (unidad1.value == "decalitro" && unidad2.value== "mililitro"){
+    resultado = numero * 1000;
+    txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "mililitro/s";
+    }
+    if (unidad1.value == "decalitro" && unidad2.value== "decalitro"){
+    resultado = numero / 1;
+    txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "decalitro/s";
+    }
+    if (unidad1.value == "decalitro" && unidad2.value== "deciloitro"){
+    resultado = numero * 100;
+    txtResultado.innerHTML = numero + " decalitro/s equivalen a " + resultado + "decilitro/s";
+    }
+    //---------------------------------decalitro----------------------//
+    //-----------------------------mililitro---------------------------//
+    if (unidad1.value == "mililitro" && unidad2.value== "Kilolitro"){
+        resultado = numero / 100000;
+        txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "Kilolitros/s";
+    }
+    if (unidad1.value == "mililitro" && unidad2.value== "litro"){
+        resultado = numero / 1000;
+        txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "litro/s";
+    }
+    if (unidad1.value == "mililitro" && unidad2.value== "Centilitro"){
+        resultado = numero / 10;
+        txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "Centiltro/s";  
+    }
+    
+    if (unidad1.value == "mililitro" && unidad2.value== "hectaltro"){
+    resultado = numero / 100000;
+    txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "hectalitro/s";
+    
+    }
+    
+    if (unidad1.value == "mililitro" && unidad2.value== "mililitro"){
+    resultado = numero * 1;
+    txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "mililitro/s";
+    }
+    if (unidad1.value == "mililitro" && unidad2.value== "decalitro"){
+    resultado = numero / 1000;
+    txtResultado.innerHTML = numero + " mililitro/s equivalen a " + resultado + "decalitro/s";
+    }
+    if (unidad1.value == "mililitro" && unidad2.value== "deciloitro"){
+    resultado = numero / 100;
+    txtResultado.innerHTML = numero + " mililitros/s equivalen a " + resultado + "decilitro/s";
+    }
+}    
 
 
 
